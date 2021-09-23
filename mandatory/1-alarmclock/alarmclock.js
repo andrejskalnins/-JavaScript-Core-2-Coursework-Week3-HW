ac1e016c5e50fdb1be88f8ad2922e9d5b625c237
andrejskalnins <andrejs.kalnins@gmail.com>
2021-09-23 23:10:56 +0100

    mandatory alarmclock - done

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,33 @@
-function setAlarm() {}
+const setAlarmBtn = document.getElementById("set");
+const stopAlarmBtn = document.getElementById("stop");
+const timeDisplay = document.getElementById("timeRemaining");
+let timeInput = document.getElementById("alarmSet");
+
+function setAlarm() {
+  setAlarmBtn.addEventListener("click", () => {
+    let timer = timeInput.value;
+
+    let countDownId = setInterval(() => {
+      if (timer >= 0) {
+        let minutes = Math.floor(timer / 60);
+        let seconds = timer % 60;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timeDisplay.innerText = `Time Remaining: ${minutes}:${seconds}`;
+        timeInput.value = "";
+      } else if (timer <= 0) {
+        clearInterval(countDownId);
+        playAlarm();
+      }
+      timer--;
+      let pause = document.querySelector("#pause");
+      pause.addEventListener("click", () => {
+        clearInterval(countDownId);
+      });
+    }, 1000);
+  });
+}
+setAlarm();
 
 // DO NOT EDIT BELOW HERE
 
